b52030fdba6e276d1bd180666611250059542000
ARUNISAI220 <arun220killer@gmail.com>
2026-04-07 12:35:09 +0530

    change l1,r1&l2r2

--- X_BOX/main.cpp
+++ X_BOX/main.cpp
@@ -1,10 +1,11 @@
 #include "raylib.h"
+#include "cmath"
 
 // Gamepad aliases
 #define XBOX_ALIAS_1 "xbox"
 #define XBOX_ALIAS_2 "x-box"
 #define PS_ALIAS_1   "playstation"
-#define PS_ALIAS_2   "sony"
+#define PS_ALIAS_2   "evofox"
 
 int main(void)
 {
@@ -17,12 +18,7 @@ int main(void)
     Texture2D texPs3Pad = LoadTexture("resources/ps3.png");
     Texture2D texXboxPad = LoadTexture("resources/xbox.png");
 
-    const float leftStickDeadzoneX = 0.1f;
-    const float leftStickDeadzoneY = 0.1f;
-    const float rightStickDeadzoneX = 0.1f;
-    const float rightStickDeadzoneY = 0.1f;
-    const float leftTriggerDeadzone = -0.9f;
-    const float rightTriggerDeadzone = -0.9f;
+    const float deadzone = 0.1f;
 
     Rectangle vibrateButton{};
 
@@ -32,7 +28,6 @@ int main(void)
 
     while (!WindowShouldClose())
     {
-        // Update
         if (IsKeyPressed(KEY_LEFT) && gamepad > 0) gamepad--;
         if (IsKeyPressed(KEY_RIGHT)) gamepad++;
 
@@ -60,15 +55,12 @@ int main(void)
             float leftTrigger = GetGamepadAxisMovement(gamepad, GAMEPAD_AXIS_LEFT_TRIGGER);
             float rightTrigger = GetGamepadAxisMovement(gamepad, GAMEPAD_AXIS_RIGHT_TRIGGER);
 
-            // Deadzones
-            if (leftStickX > -leftStickDeadzoneX && leftStickX < leftStickDeadzoneX) leftStickX = 0.0f;
-            if (leftStickY > -leftStickDeadzoneY && leftStickY < leftStickDeadzoneY) leftStickY = 0.0f;
-            if (rightStickX > -rightStickDeadzoneX && rightStickX < rightStickDeadzoneX) rightStickX = 0.0f;
-            if (rightStickY > -rightStickDeadzoneY && rightStickY < rightStickDeadzoneY) rightStickY = 0.0f;
-            if (leftTrigger < leftTriggerDeadzone) leftTrigger = -1.0f;
-            if (rightTrigger < rightTriggerDeadzone) rightTrigger = -1.0f;
+            // Deadzone
+            if (fabs(leftStickX) < deadzone) leftStickX = 0;
+            if (fabs(leftStickY) < deadzone) leftStickY = 0;
+            if (fabs(rightStickX) < deadzone) rightStickX = 0;
+            if (fabs(rightStickY) < deadzone) rightStickY = 0;
 
-            // Detect controller type
             const char* name = GetGamepadName(gamepad);
 
             if ((TextFindIndex(TextToLower(name), XBOX_ALIAS_1) > -1) ||
@@ -76,16 +68,13 @@ int main(void)
             {
                 DrawTexture(texXboxPad, 0, 0, DARKGRAY);
 
-                if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_MIDDLE)) DrawCircle(394, 89, 19, RED);
-
-                if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_MIDDLE_RIGHT)) DrawCircle(436, 150, 9, ORANGE);
-                if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_MIDDLE_LEFT)) DrawCircle(352, 150, 9, PURPLE);
-
+                // Face buttons
                 if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_RIGHT_FACE_LEFT)) DrawCircle(501, 151, 15, BLUE);
                 if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_RIGHT_FACE_DOWN)) DrawCircle(536, 187, 15, LIME);
                 if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_RIGHT_FACE_RIGHT)) DrawCircle(572, 151, 15, MAROON);
                 if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_RIGHT_FACE_UP)) DrawCircle(536, 115, 15, GOLD);
 
+                // D-Pad
                 DrawRectangle(317, 202, 19, 71, BLACK);
                 DrawRectangle(293, 228, 69, 19, BLACK);
 
@@ -94,25 +83,26 @@ int main(void)
                 if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_LEFT_FACE_LEFT)) DrawRectangle(292, 228, 25, 19, RED);
                 if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_LEFT_FACE_RIGHT)) DrawRectangle(336, 228, 26, 19, RED);
 
+                // L1 / R1 (Shoulder buttons)
+                if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_LEFT_TRIGGER_1))
+                    DrawRectangle(240, 60, 60, 20, RED);
+
+                if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_RIGHT_TRIGGER_1))
+                    DrawRectangle(500, 60, 60, 20, RED);
+
+                // L2 / R2 (Triggers as bars)
+                DrawRectangle(240, 30, (int)((leftTrigger + 1) * 50), 10, BLUE);
+                DrawText("L2", 240, 10, 10, BLACK);
+
+                DrawRectangle(500, 30, (int)((rightTrigger + 1) * 50), 10, GREEN);
+                DrawText("R2", 500, 10, 10, BLACK);
+
                 // Joysticks
                 DrawCircle(259, 152, 34, LIGHTGRAY);
                 DrawCircle(259 + (int)(leftStickX * 20), 152 + (int)(leftStickY * 20), 25, RED);
 
                 DrawCircle(461, 237, 33, LIGHTGRAY);
                 DrawCircle(461 + (int)(rightStickX * 20), 237 + (int)(rightStickY * 20), 25, RED);
-            }
-            else
-            {
-                DrawRectangleRounded(Rectangle{ 175,110,460,220 }, 0.3f, 16, DARKGRAY);
-
-                DrawCircle(345 + (int)(leftStickX * 20), 260 + (int)(leftStickY * 20), 25, RED);
-                DrawCircle(465 + (int)(rightStickX * 20), 260 + (int)(rightStickY * 20), 25, RED);
-
-                if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_MIDDLE))
-                    DrawCircle(405, 170, 10, GREEN);
-
-                if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_RIGHT_FACE_UP))
-                    DrawCircle(551, 155, 15, RED);
             }
 
             DrawText(TextFormat("AXIS COUNT: %i", GetGamepadAxisCount(gamepad)), 10, 50, 10, MAROON);
